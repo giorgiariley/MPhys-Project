34e3125d9ead2d68b2a4ef9462b8eb931b05bd78
giorgiariley <giorgiariley@gmail.com>
2026-02-12 11:57:48 +0000

    comparison of z grade and zrf

--- compare_z.py
+++ compare_z.py
@@ -7,6 +7,7 @@ CAT_FILE = "/raid/scratch/work/Griley/GALFIND_WORK/Catalogues/gdsgdn_catalogue.f
 
 zrf = Table.read(CAT_FILE)['zrf']
 zgrade = Table.read(CAT_FILE)['zgrade']
+file = Table.read(CAT_FILE)['file']
 
 plt.figure(figsize=(8, 8))
 plt.scatter(zrf, zgrade, alpha=0.5, edgecolor='k')
@@ -20,3 +21,13 @@ plt.savefig("/nvme/scratch/work/Griley/Masters/zrf_vs_zgrade.png")
 #now want to print number of points plotted that were not NaN in either zrf or zgrade
 mask = ~np.isnan(zrf) & ~np.isnan(zgrade)
 print(f"Number of points plotted: {np.sum(mask)}")
+
+#print how many rows have the word 'prism' in the file column
+prism_mask = np.array(['prism' in f for f in file])
+print(f"Number of rows with 'prism' in file column: {np.sum(prism_mask)}")
+
+v4_mask = np.array(['v4' in f for f in file])
+print(f"Number of rows with 'v4' in file column: {np.sum(v4_mask)}")
+
+mask = prism_mask & v4_mask
+print(f"Number of rows with 'prism' and 'v4' in file column: {np.sum(mask)}")
